7674691181b616acca6dda1fc8595ca1188eca7e
nikhil <nikhilv2k01@gmail.com>
2022-03-04 12:12:16 +0530

    added few things to admin

--- admin1/urls.py
+++ admin1/urls.py
@@ -4,14 +4,15 @@ from django.urls import path
 
 urlpatterns = [   
     
-    path('add',views.add,name="add_teacher"),
-    path('del',views.delete,name="delete_teacher"),
-    path('upt',views.update,name="update_teacher"),
-    path('src',views.search,name="search_teacher"),
-    path('atd',views.t_attendance,name="teacher_attendance"),
-    path('dash',views.dashboard,name="dashboard"),
-    path('astd',views.a_student,name="add_student"),
-    path('stdf',views.s_fees,name="student_fees"),
-    path('lgt',views.logout,name="logout"),
+    path('add-teacher',views.add,name="add_teacher"),
+    path('teacher-list',views.t_list,name="teacher_list"),
+    path('post-attendance',views.p_attendance,name="post_attendance"),
+    path('view-attendance',views.v_attendance,name="view_attendance"),
+    path('teacher-attendance',views.t_attendance,name="teacher_attendance"),
+    path('dashboard',views.dashboard,name="dashboard"),
+    path('add-student',views.a_student,name="add_student"),
+    path('student-fees',views.s_fees,name="student_fees"),
+    path('student-list',views.s_list,name="student_list"),
+    path('log-out',views.logout,name="logout"),
 
 ]
--- admin1/views.py
+++ admin1/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render
 
 def add(request):
     return render(request,'add_teacher.html')
-def delete(request):
-    return render(request,'delete_teacher.html')
-def update(request):
-    return render(request,'update_teacher.html')
-def search(request):
-    return render(request,'search_teacher.html')
+def t_list(request):
+    return render(request,'teacher_list.html')
+def p_attendance(request):
+    return render(request,'post_attendance.html')
+def v_attendance(request):
+    return render(request,'view_attendance.html')
 def t_attendance(request):
     return render(request,'teacher_attendance.html')
 def dashboard(request):
@@ -19,5 +19,7 @@ def a_student(request):
     return render(request,'add_student.html')
 def s_fees(request):
     return render(request,'students_fees.html')
+def s_list(request):
+    return render(request,'student_list.html')
 def logout(request):
     return render(request,'index.html')
