0530d3fc917fe83ce71d9f31852ba34d2e3b9819
nikhil <nikhilv2k01@gmail.com>
2022-03-31 19:27:24 +0530

    login page changed 3rd time

--- common/urls.py
+++ common/urls.py
@@ -3,5 +3,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [   
-    path('',views.login,name="login"),
+    path('',views.common,name="common"),
+    path('admlogin',views.login,name="admin_login"),
+    path('teacherlogin',views.teacher_login,name="teacher_login"),
+    path('studentlogin',views.student_login,name="student_login"),
 ]
--- common/views.py
+++ common/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def login(request):
+def common(request):
     return render(request,'common/index.html')
+def login(request):
+    return render(request,'common/admin_login.html')
+def teacher_login(request):
+    return render(request,'common/teacher_login.html')
+def student_login(request):
+    return render(request,'common/student_login.html')
